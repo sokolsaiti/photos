64d3058c7b1238a3807a0eef5eeb4756844458d5
Sokol Saiti <3602050+sokolsaiti@users.noreply.github.com>
2019-01-24 10:37:51 +0100

    we can now list photos on the main page

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
 import sqlite3
 
-from flask import Flask, g
+from flask import Flask, g, render_template, send_from_directory
 
 DATABASE = 'photo_repository.db'
 # UPLOAD_DIR = '/home/' + getpass.getuser() + '/uploads/photos'
 UPLOAD_DIR = 'uploads/photos'
 PHOTO_DIR = 'static/photos'
+PAGE_SIZE = 5
 
 app = Flask(__name__)
 
@@ -14,9 +15,20 @@ if not os.path.exists(UPLOAD_DIR):
     os.makedirs(UPLOAD_DIR)
 
 
+@app.route('/static/img/<path:path>')
+def send_photo(path):
+    return send_from_directory('static/img', path)
+
+
+@app.route('/static/css/<path:path>')
+def send_css(path):
+    return send_from_directory('static/css', path)
+
+
+@app.route('/')
 @app.route('/<int:page>')
 def hello_world(page=1):
-    return str(page)
+    return render_template('index.html', photo_list=get_photos(page_size=PAGE_SIZE, page=page - 1))
 
 
 @app.route('/test')
@@ -54,6 +66,16 @@ def collect_uploads():
             os.rename(UPLOAD_DIR + '/' + file, PHOTO_DIR + '/' + str(file_id) + '.JPG')
 
 
+def get_photos(page_size, page):
+    with app.app_context():
+        cursor = get_db().execute(
+            "SELECT file_id,datetime_added FROM photos  WHERE oid NOT IN ( SELECT oid FROM photos ORDER BY file_id desc LIMIT ? * ? ) ORDER BY file_id desc LIMIT ?",
+            (page_size, page, page_size,))
+        result = cursor.fetchall()
+        cursor.close()
+        return result
+
+
 def uncollect_uploads():
     uploaded_files = os.listdir(PHOTO_DIR)
     for file in uploaded_files:
@@ -64,7 +86,9 @@ def insert_photo(original_file_name):
     cursor = get_db().execute("insert into photos (original_file_name, datetime_added) values(?, datetime('now'))",
                               (original_file_name,))
     get_db().commit()
-    return cursor.lastrowid
+    last_row = cursor.lastrowid
+    cursor.close()
+    return last_row
 
 
 def query_db(query, args=(), one=False):
